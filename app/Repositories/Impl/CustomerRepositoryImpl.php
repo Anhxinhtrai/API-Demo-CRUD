90be5bbb3486c482b6c634bc7cc59cb9c5f458b5
Anhxinhtrai <anhxinhtrai69@gmail.com>
2021-06-08 11:22:20 +0700

    demo crud api

---
+++ app/Repositories/Impl/CustomerRepositoryImpl.php
@@ -0,0 +1,20 @@
+<?php
+namespace App\Repositories\Impl;
+
+
+use App\Models\Customer;
+use App\Repositories\CustomerRepository;
+use App\Repositories\Eloquent\EloquentRepository;
+
+class CustomerRepositoryImpl extends EloquentRepository  implements CustomerRepository
+{
+    /**
+     * get Model
+     * @return string
+     */
+    public function getModel()
+    {
+        $model = Customer::class;
+        return $model;
+    }
+}
